a65156c7ae39d9c3cc3f57e6856eb16cf53ffe38
srilakshmi.g <srilakshmi210799@gmail.com>
2023-04-20 18:38:26 +0530

    color code and variabels

--- echo.sh
+++ echo.sh
@@ -1,8 +1,9 @@
 echo hello world
+
 #enable color
-#syntax echo -e [colm message/e[0m'
+#syntax echo -e "/e[colCODEm message/e[0m"
 #colm -colorcode- red(31m),green(32m),yellow(33m),blue(34m),magenta(35m),cyon(36m).
 #/e[0m  --disable the color
 
-echo-e "/e[31mred color/e[0m"
-echo normal text
+echo -e "\e[31mRED COLOR\e[0m"
+echo Normal Text

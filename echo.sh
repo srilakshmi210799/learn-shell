79c2b4cf82d554fc868b6715300f5a6ddbd86d6d
srilakshmi.g <srilakshmi210799@gmail.com>
2023-04-20 14:46:17 +0530

    color code

---
+++ echo.sh
@@ -0,0 +1,9 @@
+echo hello world
+#enable color
+#syntax echo -e [colm message/e[0m'
+#colm -colorcode- red(31m),green(32m),yellow(33m),blue(34m),magenta(35m),cyon(36m).
+#/e[0m  --disable the color
+
+echo -e "/e[31mRED COLOR"
+HELLO
+NORMAL TEXT
